44c7e1d29637ab7f289b25bf01693cff3e27b3aa
Tolu Joel <toluisjoel@gmail.com>
2023-10-01 12:26:49 +0100

    Update API route for recorder app

--- screen_recorder/urls.py
+++ screen_recorder/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('recorder.urls')),
+    path('api/recorder/', include('recorder.urls')),
 ]
 
 if settings.DEBUG:
