791ed51df2157515c863d3ec49cfe394df275247
Tolu Joel <toluisjoel@gmail.com>
2023-10-02 13:49:24 +0100

    Added docs

--- recorder/urls.py
+++ recorder/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views import VideoSessionView, VideoDataView, StopVideoView, VideoDetailView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     # Start a new video recording session
@@ -13,4 +14,11 @@ urlpatterns = [
     path('session/<str:session_id>/stop/', StopVideoView.as_view(), name='stop-video'),
 
     path('session/<str:session_id>/', VideoDetailView.as_view(), name='video-detail'),
+    
+    path('schema', SpectacularAPIView.as_view(), name='schema'),
+    path(
+        '',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui'
+    ),
 ]
